ec859db5e91ac5802e5c448cd17f08568531a1c6
dan7sc <su7cheng@gmail.com>
2019-05-18 15:59:14 -0300

    Update Word Class constructor.

--- app/src/main/java/com/example/android/miwok/ColorsActivity.kt
+++ app/src/main/java/com/example/android/miwok/ColorsActivity.kt
@@ -29,14 +29,14 @@ class ColorsActivity : AppCompatActivity() {
         // Create a list of words
         val words = ArrayList<Word>()
 
-        words.add(Word("red", "weṭeṭṭi"))
-        words.add(Word("green", "chokokki"))
-        words.add(Word("brown", "ṭakaakki"))
-        words.add(Word("gray", "ṭopoppi"))
-        words.add(Word("black", "kululli"))
-        words.add(Word("white", "kelelli"))
-        words.add(Word("dusty yellow", "ṭopiisә"))
-        words.add(Word("mustartd yellow", "chiwiiṭә"))
+        words.add(Word("red", "weṭeṭṭi", R.drawable.color_red))
+        words.add(Word("green", "chokokki", R.drawable.color_green))
+        words.add(Word("brown", "ṭakaakki", R.drawable.color_brown))
+        words.add(Word("gray", "ṭopoppi", R.drawable.color_gray))
+        words.add(Word("black", "kululli", R.drawable.color_black))
+        words.add(Word("white", "kelelli", R.drawable.color_white))
+        words.add(Word("dusty yellow", "ṭopiisә", R.drawable.color_dusty_yellow))
+        words.add(Word("mustartd yellow", "chiwiiṭә", R.drawable.color_mustard_yellow))
 
 
         // Create an {@link WordAdapter}, whose data source is a list of {@link Word}s. The
