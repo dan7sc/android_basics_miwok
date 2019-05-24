b6fb582743edc3b4e8a62919c306185a92e3c452
dan7sc <su7cheng@gmail.com>
2019-05-24 15:45:37 -0300

    Add audio icon to list item layout.

--- app/src/main/java/com/example/android/miwok/NumbersActivity.kt
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.kt
@@ -13,8 +13,12 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+@file:Suppress("DEPRECATION")
+
 package com.example.android.miwok
 
+import android.content.Context
+import android.media.AudioManager
 import android.media.MediaPlayer
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
@@ -28,6 +32,34 @@ class NumbersActivity : AppCompatActivity() {
     /** Handles playback of all the sound files  */
     private var mMediaPlayer: MediaPlayer? = null
 
+    /** Handles audio focus when playing a sound file  */
+    private var mAudioManager: AudioManager? = null
+
+    /**
+     * This listener gets triggered whenever the audio focus changes
+     * (i.e., we gain or lose audio focus because of another app or device).
+     */
+    private val mOnAudioFocusChangeListener = AudioManager.OnAudioFocusChangeListener { focusChange ->
+        if (focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT || focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK) {
+            // The AUDIOFOCUS_LOSS_TRANSIENT case means that we've lost audio focus for a
+            // short amount of time. The AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK case means that
+            // our app is allowed to continue playing sound but at a lower volume. We'll treat
+            // both cases the same way because our app is playing short sound files.
+
+            // Pause playback and reset player to the start of the file. That way, we can
+            // play the word from the beginning when we resume playback.
+            mMediaPlayer!!.pause()
+            mMediaPlayer!!.seekTo(0)
+        } else if (focusChange == AudioManager.AUDIOFOCUS_GAIN) {
+            // The AUDIOFOCUS_GAIN case means we have regained focus and can resume playback.
+            mMediaPlayer!!.start()
+        } else if (focusChange == AudioManager.AUDIOFOCUS_LOSS) {
+            // The AUDIOFOCUS_LOSS case means we've lost audio focus and
+            // Stop playback and clean up resources
+            releaseMediaPlayer()
+        }
+    }
+
     /**
      * This listener gets triggered when the [MediaPlayer] has completed
      * playing the audio file.
@@ -40,6 +72,9 @@ class NumbersActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.word_list)
+
+        // Create and setup the {@link AudioManager} to request audio focus
+        mAudioManager = getSystemService(Context.AUDIO_SERVICE) as AudioManager
 
         // Create a list of words
         val words = ArrayList<Word>()
@@ -76,16 +111,26 @@ class NumbersActivity : AppCompatActivity() {
             // Get the {@link Word} object at the given position the user clicked on
             val word = words[position]
 
-            // Create and setup the {@link MediaPlayer} for the audio resource associated
-            // with the current word
-            mMediaPlayer = MediaPlayer.create(this@NumbersActivity, word.getAudioResourceId())
+            // Request audio focus so in order to play the audio file. The app needs to play a
+            // short audio file, so we will request audio focus with a short amount of time
+            // with AUDIOFOCUS_GAIN_TRANSIENT.
+            val result = mAudioManager!!.requestAudioFocus(mOnAudioFocusChangeListener,
+                    AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN_TRANSIENT)
 
-            // Start the audio file
-            mMediaPlayer!!.start()
+            if (result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
+                // We have audio focus now.
 
-            // Setup a listener on the media player, so that we can stop and release the
-            // media player once the sound has finished playing.
-            mMediaPlayer!!.setOnCompletionListener(mCompletionListener)
+                // Create and setup the {@link MediaPlayer} for the audio resource associated
+                // with the current word
+                mMediaPlayer = MediaPlayer.create(this@NumbersActivity, word.getAudioResourceId())
+
+                // Start the audio file
+                mMediaPlayer!!.start()
+
+                // Setup a listener on the media player, so that we can stop and release the
+                // media player once the sound has finished playing.
+                mMediaPlayer!!.setOnCompletionListener(mCompletionListener)
+            }
         }
     }
 
@@ -110,6 +155,10 @@ class NumbersActivity : AppCompatActivity() {
             // setting the media player to null is an easy way to tell that the media player
             // is not configured to play an audio file at the moment.
             mMediaPlayer = null
+
+            // Regardless of whether or not we were granted audio focus, abandon it. This also
+            // unregisters the AudioFocusChangeListener so we don't get anymore callbacks.
+            mAudioManager!!.abandonAudioFocus(mOnAudioFocusChangeListener)
         }
     }
 }
